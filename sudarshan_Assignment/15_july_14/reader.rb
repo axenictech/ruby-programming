6b6eed88f86ab10fe24b1e667d06c461ee89b21d
tanvi <tanvipadekar18@gmail.com>
2014-07-19 20:16:03 +0530

    Assignment

--- sudarshan_Assignment/15_july_14/reader.rb
+++
@@ -1,9 +0,0 @@
-class Student
-attr_reader :id,:name
-def initialize(id,name)
-@id=id
-@name=name
-end
-end
-student=Student.new(100,"sudarshan")
-puts "Id: #{student.id} Name: #{student.name}"
---
+++ tanvi/tanvi.rb
@@ -0,0 +1,2 @@
+i
+fhghj bgh
