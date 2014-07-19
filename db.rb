729c3ed4548ea828ae2898e2de60293275ead231
Rahul <rahul@ubuntu.ubuntu-domain>
2014-07-19 08:08:42 -0400

    connection

---
+++ db.rb
@@ -0,0 +1,23 @@
+
+require "mysql"
+begin
+     # connect to the MySQL server
+     con = Mysql.new 'localhost','root','root','anuja'
+     # get server version string and display it
+     
+     rs=con.query("select*from demo1 where id=1")
+     no_row=rs.num_rows
+     puts "the no of row #{no_row}"
+     no_row.times do
+       puts rs.fetch_row.join("\s")  
+     end
+          
+     
+rescue Mysql::Error => e
+     puts e.errno
+     puts e.error
+     
+ensure
+     # disconnect from server
+     con.close if con
+end
