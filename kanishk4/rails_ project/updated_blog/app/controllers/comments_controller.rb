ba5c455caa8102f39b7da45b570682dcc2c8ce4f
kanishk <kanish@kani.(none)>
2014-08-14 19:35:06 +0530

    updated blog operation,in rails project

--- kanishk4/rails_ project/updated_blog/app/controllers/comments_controller.rb
+++ kanishk4/rails_ project/updated_blog/app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
  
-http_basic_authenticate_with name: "rails ", password: "123", only: :destroy
+http_basic_authenticate_with name: "rail", password: "123", only: :destroy
 
  def create
   @article = Article.find(params[:article_id])
